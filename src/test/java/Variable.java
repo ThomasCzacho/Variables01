ad7700841c76a0f5d617b4e165f87e57b35efb73
Thomas Czachorowski <Thomas.Czachorowski@cdpipelinedevshop.com>
2022-08-17 11:26:27 -0400

    Initial commit

---
+++ src/test/java/Variable.java
@@ -0,0 +1,16 @@
+public class Variable {
+
+    public static void main(String[] args) {
+        String x = "Bryan Cranston";
+        System.out.println("My name is " +x);
+        System.out.println(x +" is so cool!");
+        System.out.println("Hooray " +x +"!");
+        System.out.println("Please enjoy " +x +" Appreciation "
+                + "Day! My name is " +x +". I know how excited "
+                + "everyone is to start appreciating " +x +" on " +x
+                + " Appreciation Day! " +x +", " +x +", " +x +"! Yay "
+                + x +"!!! That's me! " +x +" is the best date ever!");
+
+    }
+
+}
